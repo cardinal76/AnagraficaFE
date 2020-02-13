1dc06271bc9b6fbcaf219e7c17d91016b4953f32
Marco <marco.cardinali.lavoro@gmail.com>
2020-02-13 14:59:40 +0100

    aggiornamenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,29 +9,44 @@ import { AppComponent } from './app.component';
 import { AnagrafeModule } from './anagrafe/anagrafe.module';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
+import { AuthService } from './auth.service';
 import { HttpClientModule } from '@angular/common/http';
 
  import { HeroService } from './hero.service';
  import { HeroesComponent } from './heroes/heroes.component';
  import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
-
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+const firebaseConfig = {
+    apiKey: 'AIzaSyAwz6UJLbSLjILWobxBvqDXkOBLpoCVJ-4',
+    authDomain: 'anagraficardi.firebaseapp.com',
+    databaseURL: 'https://anagraficardi.firebaseio.com',
+    projectId: 'anagraficardi',
+    storageBucket: '',
+    messagingSenderId: '352965876755'
+};
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AnagrafeModule,
      AppRoutingModule,
-     HttpClientModule
+     HttpClientModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
 
   ],
   providers: [
-
+      AuthService
   ],
   bootstrap: [AppComponent]
 })
