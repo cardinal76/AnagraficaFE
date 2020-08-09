3f6f4f4bfedf6d16f946b2febb12be22a8b359b8
Marco Cardinali <m.cardinali@rheagroup.com>
2020-08-09 19:48:43 +0200

    material aggiunto

--- anagrafica-client/src/app/app-routing.module.ts
+++ anagrafica-client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {InsertComponent} from "./insert/insert.component";
+import {ListComponent} from "./list/list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"insert", component: InsertComponent},
+  {path:"list", component: ListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- anagrafica-client/src/app/app.module.ts
+++ anagrafica-client/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import {MatSliderModule} from "@angular/material/slider";
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { InsertComponent } from './insert/insert.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    InsertComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
